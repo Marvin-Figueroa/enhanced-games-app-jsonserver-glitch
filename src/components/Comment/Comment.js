386673e639347bdc6c41c0384584b0c55a6cfa06
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-25 11:45:13 -0600

    Change double quotes to single quotes and add Prop-Types

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,12 +1,17 @@
-import "./Comment.scss";
+import './Comment.scss';
+import PropTypes from 'prop-types';
 
 const Comment = ({ comment }) => {
   return (
-    <div className="comment">
-      <span className="comment__user">{comment.user}</span>
-      <p className="comment__text">{comment.comment}</p>
+    <div className='comment'>
+      <span className='comment__user'>{comment.user}</span>
+      <p className='comment__text'>{comment.comment}</p>
     </div>
   );
+};
+
+Comment.propTypes = {
+  comment: PropTypes.object.isRequired,
 };
 
 export default Comment;
