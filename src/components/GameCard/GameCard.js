6cbd935f7178bcb609f2643feb79dd77ce6b8242
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-25 11:46:41 -0600

    Change double quotes to single quotes and add Prop-Types

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -1,20 +1,31 @@
-import "./GameCard.scss";
-import GameRating from "../GameRating/GameRating";
+import './GameCard.scss';
+import GameRating from '../GameRating/GameRating';
+import PropTypes from 'prop-types';
 
-const GameCard = ({ id, name, rating, image, handleGameSelect }) => {
+const GameCard = ({ game, handleGameSelect }) => {
   return (
-    <article className="game-card">
-      <div className="game-card__image-container">
-        <img src={image} alt="" className="game-card__image" />
+    <article className='game-card'>
+      <div className='game-card__image-container'>
+        <img src={game.background_image} alt='' className='game-card__image' />
       </div>
-      <div className="game-card__text">
-        <h2 className="game-card__title">
-          <button onClick={() => handleGameSelect(id)}>{name}</button>
+      <div className='game-card__text'>
+        <h2 className='game-card__title'>
+          <button onClick={() => handleGameSelect(game.id)}>{game.name}</button>
         </h2>
-        <GameRating rating={rating} maxRating={5.0} />
+        <GameRating rating={game.rating} maxRating={5.0} />
       </div>
     </article>
   );
+};
+
+GameCard.propTypes = {
+  game: PropTypes.shape({
+    background_image: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  }),
+  handleGameSelect: PropTypes.func.isRequired,
 };
 
 export default GameCard;
