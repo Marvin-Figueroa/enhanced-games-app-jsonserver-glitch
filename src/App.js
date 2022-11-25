9e17fa1eabc52c5dd92d112dba5ec4878481be7d
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-25 11:43:11 -0600

    Refactor App component and change double to single quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from 'react';
 
-import HttpClient from "./services/http";
+import HttpClient from './services/http';
 
-import "./App.scss";
+import './App.scss';
 
-import GameCardsGrid from "./components/GameCardsGrid/GameCardsGrid";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import About from "./components/About/About";
-import GameDetails from "./components/GameDetails/GameDetails";
-import Pagination from "./components/Pagination/Pagination";
+import GameCardsGrid from './components/GameCardsGrid/GameCardsGrid';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import About from './components/About/About';
+import GameDetails from './components/GameDetails/GameDetails';
+import Pagination from './components/Pagination/Pagination';
 
-import HashLoader from "react-spinners/HashLoader";
+import HashLoader from 'react-spinners/HashLoader';
 
-const http = new HttpClient("https://plume-gelatinous-asp.glitch.me");
+const apiUrl = process.env.REACT_APP_API_BASE_URL;
+const resource = '/games?page=1&page_size=8&key=';
+// const resource = '/games?_page=1&_limit=8&key=';
+const apiKey = process.env.REACT_APP_API_KEY;
+// const fullUrl = apiUrl + resource + apiKey;
+
+const http = new HttpClient();
+
+// const http = new HttpClient('https://plume-gelatinous-asp.glitch.me');
 
 function App() {
+  const gamesCount = useRef();
   const [games, setGames] = useState([]);
-  const [gamesCount, setGamesCount] = useState(0);
+  // const [gamesCount, setGamesCount] = useState(0);
   const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState("games");
+  const [currentPage, setCurrentPage] = useState('games');
   const [selectedGame, setSelectedGame] = useState(null);
   const [currGamesPage, setCurrGamesPage] = useState(1);
 
@@ -33,14 +42,14 @@ function App() {
 
   async function getGames() {
     setLoading(true);
-    const res = await http.get("/games?_page=1&_limit=8", {
-      parseResponse: false,
-    });
+    const gamesResult = await http.get(apiUrl + resource + apiKey);
+    // console.log(http.baseURL);
+    // console.log(resource + apiKey);
 
-    setGamesCount(Number(res.headers.get("x-total-count")));
-    const gamesList = await res.json();
+    gamesCount.current = gamesResult.count;
+    // const gamesList = await res.json();
 
-    setGames(gamesList);
+    setGames(gamesResult.results);
     setLoading(false);
   }
 
@@ -50,8 +59,8 @@ function App() {
 
   function handleGameSelect(id) {
     const game = games.find((game) => Number(game.id) === Number(id));
-    setSelectedGame(game);
-    setCurrentPage("game");
+    setSelectedGame(game.id);
+    setCurrentPage('game');
   }
 
   function handlePageChange(page) {
@@ -59,32 +68,34 @@ function App() {
   }
 
   async function getPaginatedGames(page) {
-    const newGames = await http.get(`/games?_page=${page}&_limit=8`);
-    setGames(newGames);
+    const nextGames = await http.get(
+      apiUrl + `/games?page=${page}&page_size=8&key=` + apiKey
+    );
+    setGames(nextGames.results);
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       {loading ? (
-        <HashLoader color="#fb8500" />
+        <HashLoader color='#fb8500' />
       ) : (
         <>
           <Navbar handleClick={handleClick} currentPage={currentPage} />
           <main>
-            {(currentPage === "games" && (
+            {(currentPage === 'games' && (
               <GameCardsGrid
                 handleGameSelect={handleGameSelect}
                 games={games}
               />
             )) ||
-              (currentPage === "about" && <About />) ||
-              (currentPage === "game" && <GameDetails game={selectedGame} />)}
+              (currentPage === 'about' && <About />) ||
+              (currentPage === 'game' && <GameDetails gameId={selectedGame} />)}
           </main>
-          {currentPage === "games" && (
+          {currentPage === 'games' && (
             <Pagination
-              itemsCount={gamesCount}
+              totalItems={828995}
               pageSize={8}
-              selectedPage={currGamesPage}
+              currentPage={currGamesPage}
               onPageChange={handlePageChange}
             />
           )}
