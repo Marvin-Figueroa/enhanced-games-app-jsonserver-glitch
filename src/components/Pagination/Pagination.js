b06a417247877824e62629a90728727013c66b49
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-25 12:01:59 -0600

    Refactor Pagination component to adapt it to the new API data and add Prop-Types

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,74 +1,91 @@
-import { useEffect, useState } from "react";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import "./Pagination.scss";
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import './Pagination.scss';
+import PropTypes from 'prop-types';
+import { usePagination, DOTS } from '../../hooks/usePagination';
 
-const Pagination = ({ itemsCount, pageSize, onPageChange, selectedPage }) => {
-  const [currentPage, setCurrentPage] = useState(selectedPage);
+const Pagination = ({
+  totalItems,
+  pageSize,
+  onPageChange,
+  siblingCount = 1,
+  currentPage,
+}) => {
+  console.log('Pagination component was rendered!!!');
+  const paginationRange = usePagination({
+    currentPage,
+    totalItems,
+    siblingCount,
+    pageSize,
+  });
 
-  useEffect(() => {
-    onPageChange(currentPage);
-  }, [currentPage]);
-
-  const pagesCount = Math.ceil(itemsCount / pageSize);
-
-  const pageNumbers = new Array(pagesCount)
-    .fill(0)
-    .map((elem, index) => index + 1);
+  // If there are less than 2 times in pagination range we shall not render the component
+  if (currentPage === 0 || paginationRange.length < 2) {
+    return null;
+  }
 
   function nextPage() {
-    setCurrentPage((prevCurrentPage) => prevCurrentPage + 1);
+    onPageChange(currentPage + 1);
   }
 
   function prevPage() {
-    setCurrentPage((prevCurrentPage) => prevCurrentPage - 1);
+    onPageChange(currentPage - 1);
   }
 
-  if (pagesCount > 1) {
-    return (
-      <nav aria-label="Page navigation">
-        <ul className="pagination">
-          {currentPage !== 1 && (
-            <li className="page-item">
-              <button
-                className="page-btn"
-                onClick={() => {
-                  prevPage();
-                }}
-                aria-label="Previous"
-              >
-                <FaChevronLeft />
-              </button>
-            </li>
-          )}
-          {pageNumbers.map((pageNum) => (
-            <li key={pageNum} className="page-item">
+  let lastPage = paginationRange[paginationRange.length - 1];
+
+  return (
+    <nav aria-label='Page navigation'>
+      <ul className='pagination'>
+        <li className='page-item'>
+          <button
+            disabled={currentPage === 1}
+            className='page-btn'
+            onClick={prevPage}
+            aria-label='Previous'>
+            <FaChevronLeft />
+          </button>
+        </li>
+        {paginationRange.map((pageNum, index) => {
+          if (pageNum === DOTS) {
+            return (
+              <li key={pageNum + index} className='page-item'>
+                <button className='page-btn dots'>&#8230;</button>
+              </li>
+            );
+          }
+
+          return (
+            <li key={pageNum} className='page-item'>
               <button
                 className={
-                  pageNum === currentPage ? "page-btn current-page" : "page-btn"
+                  pageNum === currentPage ? 'page-btn current-page' : 'page-btn'
                 }
-                onClick={() => setCurrentPage(pageNum)}
-              >
+                onClick={() => onPageChange(pageNum)}>
                 {pageNum}
               </button>
             </li>
-          ))}
-          {currentPage !== pageNumbers.length && (
-            <li className="page-item">
-              <button
-                onClick={() => {
-                  nextPage();
-                }}
-                className="page-btn"
-                aria-label="Next"
-              >
-                <FaChevronRight />
-              </button>
-            </li>
-          )}
-        </ul>
-      </nav>
-    );
-  }
+          );
+        })}
+        <li className='page-item'>
+          <button
+            disabled={currentPage === lastPage}
+            onClick={nextPage}
+            className='page-btn'
+            aria-label='Next'>
+            <FaChevronRight />
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+Pagination.propTypes = {
+  totalItems: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  siblingCount: PropTypes.number,
 };
 
 export default Pagination;
