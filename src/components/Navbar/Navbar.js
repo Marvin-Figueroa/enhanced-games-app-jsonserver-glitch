f444745bb95e3f9a1fa895b364c4ed76896470ea
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-25 11:57:55 -0600

    Refactor Navbar component and Prop-Types

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,43 +1,44 @@
-import "./Navbar.scss";
-import { SiNintendogamecube } from "react-icons/si";
+import './Navbar.scss';
+import { SiNintendogamecube } from 'react-icons/si';
+import PropTypes from 'prop-types';
 
-const Navbar = (props) => {
+const Navbar = ({ currentPage, handleClick }) => {
   return (
-    <nav className="main-nav">
-      <div
-        onClick={() => props.handleClick("games")}
-        className="main-nav__logo"
-      >
+    <nav className='main-nav'>
+      <div onClick={() => handleClick('games')} className='main-nav__logo'>
         <SiNintendogamecube /> <span>LOGO</span>
       </div>
-      <ul className="main-nav__list">
-        <li className="main-nav__item">
+      <ul className='main-nav__list'>
+        <li className='main-nav__item'>
           <button
-            onClick={() => props.handleClick("games")}
+            onClick={() => handleClick('games')}
             className={
-              props.currentPage === "games"
-                ? "main-nav__link current"
-                : "main-nav__link"
-            }
-          >
+              currentPage === 'games'
+                ? 'main-nav__link current'
+                : 'main-nav__link'
+            }>
             Games
           </button>
         </li>
-        <li className="main-nav__item">
+        <li className='main-nav__item'>
           <button
-            onClick={() => props.handleClick("about")}
+            onClick={() => handleClick('about')}
             className={
-              props.currentPage === "about"
-                ? "main-nav__link current"
-                : "main-nav__link"
-            }
-          >
+              currentPage === 'about'
+                ? 'main-nav__link current'
+                : 'main-nav__link'
+            }>
             About
           </button>
         </li>
       </ul>
     </nav>
   );
+};
+
+Navbar.propTypes = {
+  currentPage: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default Navbar;
