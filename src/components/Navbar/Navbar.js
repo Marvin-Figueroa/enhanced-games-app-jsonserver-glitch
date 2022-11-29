1e8819b4466ebcd953ee1980db28d37bb53b69ea
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-28 19:33:01 -0600

    Add login logic and React.memo to the Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import './Navbar.scss';
 import { SiNintendogamecube } from 'react-icons/si';
 import PropTypes from 'prop-types';
 
-const Navbar = ({ currentPage, handleClick }) => {
+const Navbar = ({ currentPage, loggedInUser, onSignOut, handleClick }) => {
   return (
     <nav className='main-nav'>
       <div onClick={() => handleClick('games')} className='main-nav__logo'>
@@ -31,6 +32,28 @@ const Navbar = ({ currentPage, handleClick }) => {
             About
           </button>
         </li>
+        <li className='main-nav__item'>
+          {loggedInUser ? (
+            <>
+              <span className='main-nav__user'>({loggedInUser.username})</span>
+              <button
+                className='main-nav__link'
+                onClick={() => onSignOut(loggedInUser)}>
+                Log Out
+              </button>
+            </>
+          ) : (
+            <button
+              className={
+                currentPage === 'login'
+                  ? 'main-nav__link current'
+                  : 'main-nav__link'
+              }
+              onClick={() => handleClick('login')}>
+              Log In
+            </button>
+          )}
+        </li>
       </ul>
     </nav>
   );
@@ -39,6 +62,13 @@ const Navbar = ({ currentPage, handleClick }) => {
 Navbar.propTypes = {
   currentPage: PropTypes.string.isRequired,
   handleClick: PropTypes.func.isRequired,
+  loggedInUser: PropTypes.object,
+  onSignOut: PropTypes.func.isRequired,
 };
 
-export default Navbar;
+export default React.memo(
+  Navbar,
+  (prevProps, nextProps) =>
+    JSON.stringify(prevProps.loggedInUser) ===
+    JSON.stringify(nextProps.loggedInUser)
+);
