cecd5e2af50355ac14237b9a932e64120d1d13fe
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-28 19:45:38 -0600

    Refactor GameDetails component to use the games service and add React.memo

--- src/components/GameDetails/GameDetails.js
+++ src/components/GameDetails/GameDetails.js
@@ -1,72 +1,60 @@
 import './GameDetails.scss';
+
 import PropTypes from 'prop-types';
 
 import CommentsList from '../CommentsList/CommentsList';
 import GameRating from '../GameRating/GameRating';
-import { useEffect, useState } from 'react';
-import HttpClient from '../../services/http';
+import React, { useEffect, useState } from 'react';
 
-const http = new HttpClient();
+import { getGameDetails } from '../../services/games';
 
-const GameDetails = ({ gameId }) => {
-  const [game, setGame] = useState({});
-  const [comments, setComments] = useState([]);
-  const [users, setUsers] = useState([]);
+import HashLoader from 'react-spinners/HashLoader';
 
-  useEffect(() => {
-    getGameDetails(gameId);
-  }, []);
+const GameDetails = ({ gameId, user }) => {
+  const [game, setGame] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    getCommentsByGame();
-    getUsers();
+    setIsLoading(true);
+    getGameDetails(gameId).then((gameData) => {
+      setGame(gameData);
+      setIsLoading(false);
+    });
   }, []);
 
-  async function getCommentsByGame() {
-    const gameComments = await http.get(
-      process.env.REACT_APP_GLITCH_BASE_URL + `/comments?gameId=${gameId}`
-    );
-    setComments(gameComments);
-  }
-
-  async function getUsers() {
-    const allUsers = await http.get(
-      process.env.REACT_APP_GLITCH_BASE_URL + '/users'
-    );
-    setUsers(allUsers);
-  }
-
-  async function getGameDetails(gameId) {
-    const gameDetails = await http.get(
-      process.env.REACT_APP_API_BASE_URL +
-        `/games/${gameId}?key=663ba57cd49444e18a052cdc458cc5e6`
-    );
-
-    setGame(gameDetails);
+  if (isLoading) {
+    return <HashLoader color='#fb8500' />;
   }
 
   return (
     <article className='game'>
-      <img src={game.background_image} alt='' className='game__image' />
+      <img src={game?.background_image} alt='' className='game__image' />
       <div className='game__info'>
-        <h2 className='game__title'>{game.name}</h2>
-        <span className='game_date'>{game.released}</span>
+        <h2 className='game__title'>{game?.name}</h2>
+        <span className='game_date'>{game?.released}</span>
         <span className='game__rating'>
-          <GameRating rating={game.rating} maxRating={5.0} />
+          <GameRating rating={game?.rating} maxRating={5.0} />
         </span>
         <div>
           <h3>Platforms</h3>
-          <p>{game.platforms?.map((p) => p.platform.name).join(', ')}</p>
+          <p>{game?.platforms?.map((p) => p.platform.name).join(', ')}</p>
         </div>
-        <p className='game__description'>{game.description_raw}</p>
+        <p className='game__description'>
+          {game?.description_raw || 'NO HAY DESCRIPCION'}
+        </p>
       </div>
-      <CommentsList comments={comments} users={users} />
+      <CommentsList gameId={gameId} user={user} />
     </article>
   );
 };
 
 GameDetails.propTypes = {
   gameId: PropTypes.number.isRequired,
+  user: PropTypes.object,
 };
 
-export default GameDetails;
+export default React.memo(
+  GameDetails,
+  (prevProps, nextProps) =>
+    JSON.stringify(prevProps.user) === JSON.stringify(nextProps.user)
+);
